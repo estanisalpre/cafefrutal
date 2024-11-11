37726cf6e7e4e424ca03d3958db7f5d95452dc11
Estanislao Salinas <estanisprevite@hotmail.com>
2024-11-11 16:52:36 -0500

    Actualizamos ruta index

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Ruta para el archivo index.html en la raíz
 app.get('/', (req, res) => {  
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 app.get('/admin', (req, res) => {   
     res.sendFile(__dirname + '/public/views/admin.html');
